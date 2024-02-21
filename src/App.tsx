e317f4ab898e88d061b0c003bb57d3e828fe76d3
Geovane Borba <geovanne@live.com>
2024-02-20 21:27:42 -0300

    chore: add axios and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
 import { Player } from "./pages/Player";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./lib/react-query";
 
 function App() {
   return (
-    <ReduxProvider store={store}>
-      <Player />
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <Player />
+      </ReduxProvider>
+    </QueryClientProvider>
   );
 }
 
