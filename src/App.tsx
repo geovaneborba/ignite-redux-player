74344e6929a99903e16c459de640050e4710d750
Geovane Borba <geovanne@live.com>
2024-02-23 10:18:31 -0300

    chore: remove react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
 import { Player } from "./pages/Player";
-import { QueryClientProvider } from "react-query";
-import { queryClient } from "./lib/react-query";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReduxProvider store={store}>
-        <Player />
-      </ReduxProvider>
-    </QueryClientProvider>
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
   );
 }
 
