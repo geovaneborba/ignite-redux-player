f2e9e26efd7cb0daaf3f5175a58865f96fa57eac
Geovane Borba <geovanne@live.com>
2024-02-23 10:42:10 -0300

    feat: add player slice

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,67 +1,43 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
+import { api } from "../../lib/axios";
 
-const playerSlice = createSlice({
+type Course = {
+  id: number;
+  modules: Array<{
+    id: number;
+    title: string;
+    lessons: Array<{
+      id: string;
+      title: string;
+      duration: string;
+    }>;
+  }>;
+};
+
+type PlayerState = {
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
+};
+
+const initialState: PlayerState = {
+  course: null,
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+  isLoading: true,
+};
+
+export const loadCourse = createAsyncThunk("player/load", async () => {
+  const response = await api.get("/courses/1");
+
+  return response.data;
+});
+
+export const playerSlice = createSlice({
   name: "player",
-  initialState: {
-    course: {
-      modules: [
-        {
-          id: "1",
-          title: "Iniciando com React",
-          lessons: [
-            { id: "Jai8w6K_GnY", title: "CSS Modules", duration: "13:45" },
-            {
-              id: "w-DW4DhDfcw",
-              title: "Estilização do Post",
-              duration: "10:05",
-            },
-            {
-              id: "D83-55LUdKE",
-              title: "Componente: Header",
-              duration: "06:33",
-            },
-            {
-              id: "W_ATsETujaY",
-              title: "Componente: Sidebar",
-              duration: "09:12",
-            },
-            { id: "Pj8dPeameYo", title: "CSS Global", duration: "03:23" },
-            {
-              id: "8KBq2vhwbac",
-              title: "Form de comentários",
-              duration: "11:34",
-            },
-          ],
-        },
-        {
-          id: "2",
-          title: "Estrutura da aplicação",
-          lessons: [
-            {
-              id: "gE48FQXRZ_o",
-              title: "Componente: Comment",
-              duration: "13:45",
-            },
-            { id: "Ng_Vk4tBl0g", title: "Responsividade", duration: "10:05" },
-            {
-              id: "h5JA3wfuW1k",
-              title: "Interações no JSX",
-              duration: "06:33",
-            },
-            {
-              id: "1G0vSTqWELg",
-              title: "Utilizando estado",
-              duration: "09:12",
-            },
-          ],
-        },
-      ],
-    },
-    currentModuleIndex: 0,
-    currentLessonIndex: 0,
-  },
-
+  initialState,
   reducers: {
     play: (
       state,
@@ -72,23 +48,34 @@ const playerSlice = createSlice({
     },
 
     next: (state) => {
-      const currentModule = state.course.modules[state.currentModuleIndex];
+      const currentModule = state.course?.modules[state.currentModuleIndex];
       const currentLessonIndex = state.currentLessonIndex;
 
-      //* Verifica se existe uma nova aula no módulo atual
-      if (currentLessonIndex + 1 < currentModule.lessons.length) {
-        state.currentLessonIndex += 1;
-      } else {
-        //* Verifica se existe um novo módulo
-        if (state.currentModuleIndex + 1 < state.course.modules.length) {
-          state.currentModuleIndex += 1; //* Muda para o próximo módulo
-          state.currentLessonIndex = 0; //* Muda para a primeira aula do módulo
+      if (currentModule && state.course) {
+        //* Verifica se existe uma nova aula no módulo atual
+        if (currentLessonIndex + 1 < currentModule.lessons.length) {
+          state.currentLessonIndex += 1;
         } else {
-          //* finalizou todas aulas
-          console.log("No more lessons available.");
+          //* Verifica se existe um novo módulo
+          if (state.currentModuleIndex + 1 < state.course.modules.length) {
+            state.currentModuleIndex += 1; //* Muda para o próximo módulo
+            state.currentLessonIndex = 0; //* Muda para a primeira aula do módulo
+          } else {
+            //* finalizou todas aulas
+            console.log("No more lessons available.");
+          }
         }
       }
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadCourse.fulfilled, (state, action) => {
+      state.course = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true;
+    });
   },
 });
 
@@ -99,8 +86,8 @@ export const usePlayerProgress = () => {
   return useAppSelector((state) => {
     const { currentLessonIndex, currentModuleIndex } = state.player;
 
-    const currentModule = state.player.course.modules[currentModuleIndex];
-    const currentLesson = currentModule.lessons[currentLessonIndex];
+    const currentModule = state.player.course?.modules[currentModuleIndex];
+    const currentLesson = currentModule?.lessons[currentLessonIndex];
 
     return {
       currentModule,
