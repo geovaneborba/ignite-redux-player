951bd341e54d6345ef264caad862035fcc36a5f8
Geovane Borba <geovanne@live.com>
2024-02-24 17:18:09 -0300

    fix: ensure currentLesson and currentModule are defined before rendering
    - Add condition to check if currentLesson and currentModule are defined before rendering their titles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,14 @@ export function Header() {
 
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">
-        Módulo "{currentModule.title}"
-      </span>
+      {currentLesson && currentModule && (
+        <>
+          <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+          <span className="text-sm text-zinc-400">
+            Módulo "{currentModule.title}"
+          </span>
+        </>
+      )}
     </div>
   );
 }
